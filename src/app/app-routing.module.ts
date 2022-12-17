21407ea19f967d61f562ff16a50bab7dbb992130
Krystian Osinski <osinski.krystian.1999@gmail.com>
2022-12-17 14:23:22 +0100

    Add favicon.ico | add components: login/nav-bar/portfolio/student and teacher view | add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentViewComponent } from './student-view/student-view.component';
+import { TeacherViewComponent } from './teacher-view/teacher-view.component';
+import { LoginComponent } from './login/login.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'student-view' , component: StudentViewComponent},
+  {path: 'teacher-view' , component: TeacherViewComponent},
+  {path: 'login' , component: LoginComponent},
+  {path: 'portfolio' , component: PortfolioComponent},
+  {path: '' , redirectTo: '/portfolio',pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
